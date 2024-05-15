3fb8d3cbae1191390314b60b25a88d81b5c2a97c
Justin Mathews <github@justinmathews.ca>
2024-05-14 20:34:52 -0400

    playing around with the first script

--- Coursework/Coursework_4/ForLoopsAndSequences1.py
+++ Coursework/Coursework_4/ForLoopsAndSequences1.py
@@ -3,16 +3,26 @@ from scamp import *
 s = Session()
 
 violin = s.new_part("violin")
+viola = s.new_part("viola")
+cello = s.new_part("cello")
 
-for p in range(55, 80):
-    violin.play_note(p, 0.7, 0.25)
+for pitch in range(55, 80):
+    print(f"{pitch} ", end="")
+    violin.play_note(pitch, 0.7, 0.25)
+print("\n")
 
 wait(1)
 
-for p in range(55, 80, 3):
-    violin.play_note(p, 0.7, 0.25)
+for pitch in range(55, 80, 3):
+    print(f"{pitch} ", end="")
+    cello.play_note(pitch - 8 - 12, 0.7, 0.25, blocking=False)
+    viola.play_note(pitch - 5, 0.7, 0.25, blocking=False)
+    violin.play_note(pitch, 0.7, 0.25)
+print("\n")
 
 wait(1)
 
-for p in [60, 67, 65, 55, 89]:
-    violin.play_note(p, 0.7, 0.25)
+for pitch in [59, 66, 64, 54, 88, 60, 67, 65, 55, 89, 61, 68, 66, 56, 90]:
+    print(f"{pitch} ", end="")
+    viola.play_note(pitch, 0.7, 0.25)
+print("\n")
