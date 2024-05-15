23e7cacbff7b08cb56691672cfbd1f8724dae30f
Justin Mathews <github@justinmathews.ca>
2024-05-14 20:51:28 -0400

    playing around with the third script

--- Coursework/Coursework_4/ForLoopsAndSequences3.py
+++ Coursework/Coursework_4/ForLoopsAndSequences3.py
@@ -6,15 +6,21 @@ piano = s.new_part("piano")
 organ = s.new_part("organ")
 
 for pitch in range(50, 67):
-    if pitch < 58:
+    if pitch > 62:
         piano.play_note(pitch, 0.7, 0.25)
     else:
         organ.play_note(pitch, 0.7, 0.25)
+        organ.play_note(pitch + 4, 0.7, 0.25 / 2)
+        organ.play_note(pitch + 7, 0.7, 0.25 / 2)
 
 wait(1)
 
 for pitch in range(50, 67):
-    if pitch % 2 == 0:
+    if pitch % 5 == 1:
         piano.play_note(pitch, 0.7, 0.25)
+        piano.play_note(pitch - 5, 0.7, 0.125)
+        piano.play_note(pitch - 8, 0.7, 0.125)
     else:
         organ.play_note(pitch, 0.7, 0.25)
+    organ.play_note(pitch + 12, 0.7, 0.5, blocking=False)
+    piano.play_note(pitch, 0.7, 0.25)
