d490fbb204058370c2c20d6056904ca582dfa719
Justin Mathews <github@justinmathews.ca>
2024-05-26 00:10:58 -0400

    refactored into instrument-specific functions with forks

--- Coursework/Coursework_7/putting_it_all_together.py
+++ Coursework/Coursework_7/putting_it_all_together.py
@@ -45,6 +45,15 @@ DRUM_KITS_I_LIKE = [2, 3, 5, 8, 9]
 SCRATCH_KITS_I_LIKE = [1, 2, 7, 9, 10, 13, 17]
 
 
+def print_count(beats=None, denominator=None):
+    if not denominator:
+        denominator = 2
+    for beat in range(beats, 0, -1):
+        print(f" {beat} ", end="")
+        wait(1 / denominator)
+    print(" ", end="")
+
+
 def get_session(tempo=120, name=None) -> Session:
     if name:
         playback_settings.recording_file_path = name
@@ -88,8 +97,25 @@ def setup(new_parts):
         return main_session
 
 
-def intro_scratch(parts):
-    print(f"\rIntro", end="")
+def false_start_bass(parts):
+    bass = parts["bass"]["part"]
+    bass.play_note(kick(), 1, 2)
+
+    wait(1)
+
+    bass.play_note(kick(), 1, 2)
+
+
+def false_start_drums(parts):
+    drum_kit = parts["drum_kit"]["part"]
+    drum_kit.play_note(kick(), 1, 2)
+
+    wait(1)
+
+    drum_kit.play_note(kick(), 1, 2)
+
+
+def false_start_scratch(parts):
     scratch = parts["scratch"]["part"]
     scratch.play_note(48, 1 / 8, 2)
 
@@ -97,93 +123,135 @@ def intro_scratch(parts):
 
     scratch.play_note(48, 1 / 8, 2)
 
-    print(f" 1", end="")
+
+def intro_bass(parts):
+    bass = parts["bass"]["part"]
+
+    # beat 1
+    bass.play_note(kick(), 1, 1)
+
+    # beat 2
+    wait(1)
+
+    # beat 3
+    bass.play_note(kick(), 1, 1)
+
+    # beat 4
+    bass.play_note(kick(), 1, 1 / 8, blocking=False)
+    bass.play_note(crash(), 1, 1 / 8, blocking=False)
+    wait(1 / 4)
+    bass.play_note(kick(), 1, 1 / 8, blocking=False)
+    bass.play_note(crash(), 1, 1 / 8, blocking=False)
+    wait(1 / 4)
+    bass.play_note(kick(), 1, 1 / 8, blocking=False)
+    bass.play_note(crash(), 1, 1 / 8, blocking=False)
+    wait(1 / 4)
+    bass.play_note(kick(), 1, 1 / 8, blocking=False)
+    bass.play_note(crash(), 1, 1 / 8, blocking=False)
+    wait(1 / 4)
+
+
+def intro_drums(parts):
+    drum_kit = parts["drum_kit"]["part"]
+
+    # beat 1
+    drum_kit.play_note(kick(), 1, 1)
+
+    # beat 2
+    wait(1)
+
+    # beat 3
+    drum_kit.play_note(kick(), 1, 1)
+
+    # beat 4
+    drum_kit.play_note(kick(), 1, 1 / 2, blocking=False)
+    drum_kit.play_note(crash(), 1, 1 / 2, blocking=False)
+    wait(1 / 4)
+    drum_kit.play_note(kick(), 1, 1 / 2, blocking=False)
+    drum_kit.play_note(crash(), 1, 1 / 2, blocking=False)
+    wait(1 / 4)
+    drum_kit.play_note(kick(), 1, 1 / 2, blocking=False)
+    drum_kit.play_note(crash(), 1, 1 / 2, blocking=False)
+    wait(1 / 4)
+    drum_kit.play_note(kick(), 1, 1 / 2, blocking=False)
+    drum_kit.play_note(crash(), 1, 1 / 2, blocking=False)
+    wait(1 / 4)
+
+
+def intro_scratch(parts):
+    scratch = parts["scratch"]["part"]
+
+    # beat 1
     scratch.play_note(60, 1 / 8, 1 / 4)
     scratch.play_note(62, 2 / 8, 1 / 4)
     scratch.play_note(64, 3 / 8, 1 / 4)
     scratch.play_note(65, 4 / 8, 1 / 4)
 
-    print(f" 2", end="")
+    # beat 2
     scratch.play_note(67, 5 / 8, 1 / 4)
     scratch.play_note(69, 6 / 8, 1 / 4)
     scratch.play_note(71, 7 / 8, 1 / 4)
     scratch.play_note(72, 8 / 8, 1 / 4)
 
-    print(f" 3", end="")
+    # beat 3
     scratch.play_note(72, 1 / 8, 1 / 4)
     scratch.play_note(74, 2 / 8, 1 / 4)
     scratch.play_note(76, 3 / 8, 1 / 4)
     scratch.play_note(77, 4 / 8, 1 / 4)
 
-    print(f" 4", end="")
+    # beat 4
     scratch.play_note(79, 5 / 8, 1 / 4)
     scratch.play_note(81, 6 / 8, 1 / 4)
     scratch.play_note(83, 7 / 8, 1 / 4)
     scratch.play_note(84, 8 / 8, 1 / 4)
 
 
-def intro_drums(parts):
-    print(f"\rIntro", end="")
-    drum_kit = parts["drum_kit"]["part"]
-    drum_kit.play_note(kick(), 1, 2)
-
-    wait(1)
-
-    drum_kit.play_note(kick(), 1, 2)
-
-    print(f" 1", end="")
-    drum_kit.play_note(kick(), 1, 1)
-
-    print(f" 2", end="")
-    wait(1)
-
-    print(f" 3", end="")
-    drum_kit.play_note(kick(), 1, 1)
-
-    print(f" 4", end="")
-    drum_kit.play_note(kick(), 1, 1 / 2, blocking=False)
-    drum_kit.play_note(crash(), 1, 1 / 2, blocking=False)
-    wait(1 / 4)
-    drum_kit.play_note(kick(), 1, 1 / 2, blocking=False)
-    drum_kit.play_note(crash(), 1, 1 / 2, blocking=False)
-    wait(1 / 4)
-    drum_kit.play_note(kick(), 1, 1 / 2, blocking=False)
-    drum_kit.play_note(crash(), 1, 1 / 2, blocking=False)
-    wait(1 / 4)
-    drum_kit.play_note(kick(), 1, 1 / 2, blocking=False)
-    drum_kit.play_note(crash(), 1, 1 / 2, blocking=False)
-    wait(1 / 4)
-
-
-def section_a(parts):
-    print(f"\rSection A", end="")
-    drum_kit = parts["drum_kit"]["part"]
+def section_a_bass(parts):
     bass = parts["bass"]["part"]
-    scratch = parts["scratch"]["part"]
 
-    print(f" 1", end="")
+    # beat 1
     bass.play_note(48, 1 / 8, 1, blocking=False)
+    wait(1 / 2)
+
+    # beat 2
+    wait(1 / 2)
+
+    # beat 3
+    wait(1 / 4)
+    bass.play_note(48, 1 / 8, uniform(1 / 8, 3 / 8), blocking=False)
+    wait(1 / 4)
+
+    # beat 4
+    wait(1 / 4)
+
+    bass.play_note(46, 1 / 8, 1 / 4, blocking=False)
+    wait(1 / 4)
+
+
+def section_a_drums(parts):
+    drum_kit = parts["drum_kit"]["part"]
+
+    # beat 1
     drum_kit.play_note(crash(), 1, 1 / 2, blocking=False)
     drum_kit.play_note(kick(), 1, 1 / 2, blocking=False)
     drum_kit.play_note(CLOSED_HAT, 1 / 8, 1 / 4)
     drum_kit.play_note(hat(), 3 / 4, 1 / 4)
 
-    print(f" 2", end="")
+    # beat 2
     drum_kit.play_note(snare(), 3 / 4, 1 / 2, blocking=False)
     if choice([True, False]):
         drum_kit.play_note(kick(), 1 / 4, 1 / 2, blocking=False)
     drum_kit.play_note(CLOSED_HAT, 1 / 8, 1 / 4)
     drum_kit.play_note(hat(), 3 / 4, 1 / 4)
 
-    print(f" 3", end="")
+    # beat 3
     drum_kit.play_note(kick(), 2 / 4, 1 / 2, blocking=False)
     drum_kit.play_note(CLOSED_HAT, 1 / 8, 1 / 4)
-    bass.play_note(48, 1 / 8, uniform(1 / 8, 3 / 8), blocking=False)
     if choice([True, False]):
         drum_kit.play_note(snare(), 3 / 4, 1 / 4, blocking=False)
     drum_kit.play_note(hat(), 3 / 4, 1 / 4)
 
-    print(f" 4", end="")
+    # beat 4
     drum_kit.play_note(snare(), 3 / 4, 1 / 2, blocking=False)
     if choice([True, False]):
         drum_kit.play_note(kick(), 1 / 4, 1 / 4, blocking=False)
@@ -191,29 +259,68 @@ def section_a(parts):
         drum_kit.play_note(72, 1 / 8, 1 / 4, blocking=False)
     drum_kit.play_note(CLOSED_HAT, 1 / 8, 1 / 4)
 
-    bass.play_note(46, 1 / 8, 1 / 4, blocking=False)
     if choice([True, False]):
         drum_kit.play_note(snare(), 3 / 4, 1 / 4, blocking=False)
     if choice([True, False]):
         drum_kit.play_note(kick(), 3 / 4, 1 / 2, blocking=False)
     if choice([True, False]):
-        scratch.play_note(84, 1 / 8, 3 / 4, blocking=False)
-    if choice([True, False]):
         drum_kit.play_note(crash(), 1, 1 / 2, blocking=False)
     drum_kit.play_note(hat(), 3 / 4, 1 / 4)
 
 
-def section_b(parts):
-    print(f"\rSection B", end="")
-    drum_kit = parts["drum_kit"]["part"]
-    bass = parts["bass"]["part"]
+def section_a_scratch(parts):
     scratch = parts["scratch"]["part"]
 
-    print(f" 1", end="")
+    # beat 1
+    wait(1 / 2)
+
+    # beat 2
+    wait(1 / 2)
+
+    # beat 3
+    wait(1 / 2)
+
+    # beat 4
+    wait(1 / 4)
+
+    if choice([True, False]):
+        scratch.play_note(84, 1 / 8, 3 / 4, blocking=False)
+    wait(1 / 4)
+
+
+def section_b_bass(parts):
+    bass = parts["bass"]["part"]
+
+    # beat 1
     bass.play_note(60, 1 / 8, uniform(3 / 4, 1), blocking=False)
+    wait(1)
+
+    # beat 2
+    wait(1 / 2)
+
+    bass.play_note(59, 1 / 8, uniform(1 / 4, 3 / 4), blocking=False)
+    wait(1 / 2)
+
+    # beat 3
+    wait(1 / 2)
+
+    bass.play_note(58, 1 / 8, uniform(3 / 8, 5 / 8), blocking=False)
+    wait(1 / 2)
+
+    # beat 4
+    bass.play_note(57, 1 / 8, uniform(3 / 8, 5 / 8), blocking=False)
+    wait(1 / 2)
+
+    bass.play_note(56, 1 / 8, 1 / 2, blocking=False)
+    wait(1 / 2)
+
+
+def section_b_drums(parts):
+    drum_kit = parts["drum_kit"]["part"]
+
+    # beat 1
     drum_kit.play_note(kick(), 2 / 4, 1, blocking=False)
     drum_kit.play_note(OPEN_HAT, 3 / 4, 1 / 2, blocking=False)
-    scratch.play_note(72, 8 / 8, 1 / 4, blocking=False)
     drum_kit.play_note(ride(), random(), 1 / 4)
     drum_kit.play_note(ride(), random(), 1 / 4)
 
@@ -221,94 +328,133 @@ def section_b(parts):
     drum_kit.play_note(snare(), 3 / 4, 1 / 2, blocking=False)
     drum_kit.play_note(ride(), random(), 1 / 4)
 
-    print(f" 2", end="")
-    scratch.play_note(72, 8 / 8, 1 / 2, blocking=False)
+    # beat 2
     drum_kit.play_note(ride(), random(), 1 / 4)
     drum_kit.play_note(ride(), random(), 1 / 4)
 
-    bass.play_note(59, 1 / 8, uniform(1 / 4, 3 / 4), blocking=False)
     drum_kit.play_note(kick(), 2 / 4, 1, blocking=False)
-    scratch.play_note(75, 8 / 8, 1 / 4, blocking=False)
     drum_kit.play_note(ride(), random(), 1 / 4)
     drum_kit.play_note(ride(), random(), 1 / 4)
 
-    print(f" 3", end="")
+    # beat 3
     drum_kit.play_note(hat(), 3 / 4, 1 / 2, blocking=False)
     drum_kit.play_note(ride(), random(), 1 / 4)
     drum_kit.play_note(ride(), random(), 1 / 4)
 
-    bass.play_note(58, 1 / 8, uniform(3 / 8, 5 / 8), blocking=False)
     drum_kit.play_note(snare(), 3 / 4, 1 / 2, blocking=False)
-    scratch.play_note(72, 8 / 8, 1 / 2, blocking=False)
     drum_kit.play_note(ride(), random(), 1 / 4)
     drum_kit.play_note(snare(), 3 / 4, 1 / 2, blocking=False)
     drum_kit.play_note(ride(), random(), 1 / 4)
 
-    print(f" 4", end="")
-    bass.play_note(57, 1 / 8, uniform(3 / 8, 5 / 8), blocking=False)
+    # beat 4
     drum_kit.play_note(kick(), 2 / 4, 1, blocking=False)
     drum_kit.play_note(crash(), random(), 1 / 4)
     drum_kit.play_note(ride(), random(), 1 / 4)
 
-    bass.play_note(56, 1 / 8, 1 / 2, blocking=False)
     drum_kit.play_note(ride(), random(), 1 / 4)
     drum_kit.play_note(snare(), 3 / 4, 1 / 2, blocking=False)
-    scratch.play_note(78, 8 / 8, 1 / 2, blocking=False)
     drum_kit.play_note(ride(), random(), 1 / 4)
 
 
-def outro(parts):
-    print(f"\rOutro", end="")
-
-    drum_kit = parts["drum_kit"]["part"]
-    bass = parts["bass"]["part"]
+def section_b_scratch(parts):
     scratch = parts["scratch"]["part"]
 
-    print(f" 1", end="")
-    drum_kit.play_note(snare(), 2 / 4, 1 / 4, blocking=False)
-    drum_kit.play_note(ride(), 2 / 4, 1 / 4, blocking=False)
+    # beat 1
+    scratch.play_note(72, 8 / 8, 1 / 4, blocking=False)
+    wait(1)
+
+    # beat 2
+    scratch.play_note(72, 8 / 8, 1 / 2, blocking=False)
+    wait(1 / 2)
+
+    scratch.play_note(75, 8 / 8, 1 / 4, blocking=False)
+    wait(1 / 2)
+
+    # beat 3
+    wait(1 / 2)
+
+    scratch.play_note(72, 8 / 8, 1 / 2, blocking=False)
+    wait(1 / 2)
+
+    # beat 4
+    wait(1 / 2)
+
+    wait(1 / 4)
+    scratch.play_note(78, 8 / 8, 1 / 2, blocking=False)
+    wait(1 / 4)
+
+
+def outro_bass(parts):
+    bass = parts["bass"]["part"]
+
+    # beat 1
     bass.play_note(60, 2 / 4, 1 / 4)
-    drum_kit.play_note(kick(), 2 / 4, 1 / 4, blocking=False)
-    drum_kit.play_note(ride(), 2 / 4, 1 / 4, blocking=False)
     bass.play_note(48, 2 / 4, 1 / 4)
 
-    print(f" 2", end="")
-    drum_kit.play_note(snare(), 2 / 4, 1 / 4, blocking=False)
-    drum_kit.play_note(crash(), 2 / 4, 1 / 4, blocking=False)
+    # beat 2
     bass.play_note(60, 2 / 4, 1 / 4)
-    drum_kit.play_note(kick(), 2 / 4, 1 / 4, blocking=False)
     bass.play_note(48, 2 / 4, 1 / 4)
 
-    print(f" 3", end="")
-    drum_kit.play_note(snare(), 2 / 4, 1 / 4, blocking=False)
-    drum_kit.play_note(ride(), 2 / 4, 1 / 4, blocking=False)
-    bass.play_note(60, 2 / 4, 1 / 4)
-    drum_kit.play_note(kick(), 2 / 4, 1 / 4, blocking=False)
-    drum_kit.play_note(snare(), 2 / 4, 1 / 4, blocking=False)
-    bass.play_note(48, 2 / 4, 1 / 4)
+    # beat 3
+    bass.play_note(60, 2 / 4, 1 / 6)
+    bass.play_note(53, 2 / 4, 1 / 6)
+    bass.play_note(48, 2 / 4, 1 / 6)
 
-    print(f" 4", end="")
-    drum_kit.play_note(kick(), 2 / 4, 1 / 4, blocking=False)
-    drum_kit.play_note(snare(), 2 / 4, 1 / 4, blocking=False)
-    drum_kit.play_note(crash(), 2 / 4, 1 / 4, blocking=False)
-    drum_kit.play_note(crash(), 2 / 4, 1 / 4, blocking=False)
+    # beat 4
     bass.play_note(36, 2 / 4, 1 / 2)
 
 
-def play_beat(parts):
-    for _ in range(REPS):
-        for _ in range(SECTION_A_REPS):
-            section_a(parts)
-        for _ in range(SECTION_B_REPS):
-            section_b(parts)
-    for _ in range(SECTION_A_REPS // 2):
-        section_a(parts)
-    outro(parts)
+def outro_drums(parts):
+    drum_kit = parts["drum_kit"]["part"]
+
+    # beat 1
+    drum_kit.play_note(snare(), 2 / 4, 1 / 4, blocking=False)
+    drum_kit.play_note(ride(), 2 / 4, 1 / 4, blocking=False)
+    wait(1 / 4)
+    drum_kit.play_note(kick(), 2 / 4, 1 / 4, blocking=False)
+    drum_kit.play_note(ride(), 2 / 4, 1 / 4, blocking=False)
+    wait(1 / 4)
+
+    # beat 2
+    drum_kit.play_note(snare(), 2 / 4, 1 / 4, blocking=False)
+    drum_kit.play_note(crash(), 2 / 4, 1 / 4, blocking=False)
+    wait(1 / 4)
+    drum_kit.play_note(kick(), 2 / 4, 1 / 4, blocking=False)
+    wait(1 / 4)
+
+    # beat 3
+    drum_kit.play_note(snare(), 2 / 4, 1 / 4, blocking=False)
+    drum_kit.play_note(ride(), 2 / 4, 1 / 4, blocking=False)
+    wait(1 / 4)
+    drum_kit.play_note(kick(), 2 / 4, 1 / 4, blocking=False)
+    drum_kit.play_note(snare(), 2 / 4, 1 / 4, blocking=False)
+    wait(1 / 4)
+
+    # beat 4
+    drum_kit.play_note(kick(), 2 / 4, 1 / 4, blocking=False)
+    drum_kit.play_note(snare(), 2 / 4, 1 / 4, blocking=False)
+    drum_kit.play_note(crash(), 2 / 4, 1 / 4, blocking=False)
+    drum_kit.play_note(crash(), 2 / 4, 1 / 4, blocking=False)
+    wait(1 / 2)
+
+
+def outro_scratch(parts):
+    scratch = parts["scratch"]["part"]
+
+    # beat 1
+    wait(1 / 2)
+
+    # beat 2
+    wait(1 / 2)
+
+    # beat 3
+    wait(1 / 2)
+
+    # beat 4
+    wait(1 / 2)
 
 
 def play_melody(parts):
-    print(f"\rMelody", end="")
-
     lead_synth = parts["lead_synth"]["part"]
 
     tonic = 55
@@ -325,7 +471,6 @@ def play_melody(parts):
         approximate_melody_length = len(pitches) * len(intervals) * 1.5 * sum(lengths) / len(lengths)
 
         chord = [tonic + (12 * octave) for octave in octaves]
-        print(f"{tonic=}, {chord=}, {pitches=}")
 
         for octave in octaves:
             lead_synth.play_note(tonic + (12 * octave), 0.25, approximate_melody_length, blocking=False)
@@ -359,6 +504,51 @@ def play_melody(parts):
             break
 
 
+def false_start(s: Session, parts):
+    print(f"\rStarting", end="")
+    fork(print_count, [5, 1])
+    fork(false_start_bass, [parts])
+    fork(false_start_drums, [parts])
+    fork(false_start_scratch, [parts])
+    s.wait_for_children_to_finish()
+
+
+def intro(s: Session, parts):
+    print(f"\rIntro", end="")
+    fork(print_count, [8, 2])
+    fork(intro_bass, [parts])
+    fork(intro_drums, [parts])
+    fork(intro_scratch, [parts])
+    s.wait_for_children_to_finish()
+
+
+def section_a(s: Session, parts):
+    print(f"\rSection A", end="")
+    fork(print_count, [4, 2])
+    fork(section_a_drums, [parts])
+    fork(section_a_bass, [parts])
+    fork(section_a_scratch, [parts])
+    s.wait_for_children_to_finish()
+
+
+def section_b(s: Session, parts):
+    print(f"\rSection B", end="")
+    fork(print_count, [8, 2])
+    fork(section_b_drums, [parts])
+    fork(section_b_bass, [parts])
+    fork(section_b_scratch, [parts])
+    s.wait_for_children_to_finish()
+
+
+def outro(s: Session, parts):
+    print(f"\rOutro", end="")
+    fork(print_count, [7, 4])
+    fork(outro_bass, [parts])
+    fork(outro_drums, [parts])
+    fork(outro_scratch, [parts])
+    s.wait_for_children_to_finish()
+
+
 def main():
     parts = {
         "drum_kit": {
@@ -381,12 +571,17 @@ def main():
 
     s = setup(parts)
 
-    fork(intro_drums, [parts])
-    fork(intro_scratch, [parts])
-    s.wait_for_children_to_finish()
-    fork(play_beat, [parts])
-    fork(play_melody, [parts])
-    s.wait_for_children_to_finish()
+    intro(s, parts)
+
+    for rep in range(REPS):
+        for _ in range(SECTION_A_REPS - 2 * rep):
+            section_a(s, parts)
+        for _ in range(SECTION_B_REPS - 2 * rep):
+            section_b(s, parts)
+    for _ in range(SECTION_A_REPS // 2):
+        section_a(s, parts)
+
+    outro(s, parts)
 
 
 if __name__ == '__main__':
